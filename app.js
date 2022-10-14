711121298a8d1c082f044afb1b3a23fb1f2ba3cf
Alex Sumoski <alexsumoski@gmail.com>
2022-10-13 20:59:31 -0400

    scatterplot started

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-async function getData() {
-    const data =  await d3.json('./data.json')
+async function draw() {
+    const dataset = await d3.json('data.json')
 
-    console.log(data);
+    let dimensions = {
+        width: 800,
+        height: 800,
+        margin: {
+            top: 50,
+            bottom: 50,
+            left: 50,
+            right: 50
+        }
+    }
+
+    const svg = d3.select('#chart')
+        .append('svg')
+        .attr('width', dimensions.width)
+        .attr('height', dimensions.height)
+
+    const container = svg.append('g')
+        .attr('transform', 
+            `translate(${dimensions.margin.left}, ${dimensions.margin.top})`)
+
+    container.append('circle')
+        .attr('r', 15)
 }
 
-getData()
+draw()
