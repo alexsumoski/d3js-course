705575060a797bc5b61e932fe741bf9f9de4c666
Alex Sumoski <alexsumoski@gmail.com>
2022-10-22 01:26:30 -0400

    quantize scale

--- heatmap.js
+++ heatmap.js
@@ -1,6 +1,7 @@
 async function draw(el, scale) {
 
     const dataset = await d3.json('data2.json')
+    dataset.sort((a, b) => a - b)
 
     let dimensions = {
         width: 600,
@@ -15,8 +16,12 @@ async function draw(el, scale) {
         colorScale = d3.scaleLinear()
             .domain(d3.extent(dataset))
             .range(['white', 'red'])
+    } else if (scale === 'quantize') {
+        colorScale = d3.scaleQuantize()
+            .domain(d3.extent(dataset))
+            .range(['white', 'pink', 'red'])
     }
-    
+     
     const svg = d3.select(el)
         .append("svg")
         .attr("width", dimensions.width)
@@ -37,4 +42,5 @@ async function draw(el, scale) {
 }
 
 draw('#heatmap1', 'linear')
+draw('#heatmap2', 'quantize')
       
